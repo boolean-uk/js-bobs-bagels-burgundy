479f6ea0e797684eb5d5e10e4ca9b6bb5d2165c9
Koci H <husnukoci94@gmail.com>
2023-06-19 18:48:03 +0300

    core is done.

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -1,126 +1,115 @@
-let basket;
-const smallBasket = 5;
-const mediumBasket = 10;
-const largeBasket = 15;
+const Basket = require("../src/basket.js");
+const Item = require("../src/item.js");
 
-beforeEach(() => {
-  basket = new Basket();
-});
+describe("Basket", () => {
+  let basket;
+  const smallBasket = 5;
+  const mediumBasket = 10;
+  const largeBasket = 15;
+  beforeEach(() => {
+    basket = new Basket();
+  });
 
-//Test 1
-it("Get all basket", () => {
-  const expected = [];
-  let getBasket = basket.getBasket();
-  expect(getBasket).toEqual(expected);
-});
+  //Test 1
+  it("Get all basket", () => {
+    const expected = [];
+    let getBasket = basket.getBasket();
+    expect(getBasket).toEqual(expected);
+  });
 
-// Test Added
-it("cant add item that doesnt exist", () => {
-  const expected = "this item does not exist";
+  //* Test added
+  it("cant add item that doesnt exist", () => {
+    const expected = "this item does not exist";
+    let result = basket.addItem("bagelio", 1);
+    let bagelInBasket = basket.getBasket();
+    expect(result).toEqual(expected);
+    expect(bagelInBasket).toEqual([]);
+  });
 
-  let result = basket.addItem("bagelio", 1);
-  let bagelInBasket = basket.getBasket();
-  expect(result).toEqual(expected);
-  expect(bagelInBasket).toEqual([]);
-});
+  //Test 2
+  it("Add items to basket", () => {
+    const newItem1 = new Item("bagel", 1, 2.99);
+    const newItem2 = new Item("brownie", 3, 3.99);
+    const expected = [newItem1, newItem2];
+    basket.addItem("bagel", 1);
+    basket.addItem("brownie", 3);
+    let result = basket.getBasket();
+    expect(result).toEqual(expected);
+  });
 
+  //Test 3
+  it("Remove bagel from basket", () => {
+    const expected = [new Item("brownie", 3, 3.99)];
 
-//Test 2
-it("Add items to basket", () => {
-  const newItem1 = new Item("bagel", 1, 2.99);
-  const newItem2 = new Item("brownie", 3, 3.99);
+    basket.addItem("bagel", 1);
+    basket.addItem("brownie", 3);
+    basket.removeItem("bagel");
+    let result = basket.getBasket();
+    expect(result).toEqual(expected);
+  });
 
-  const expected = [newItem1, newItem2];
+  it("Alert when basket is full", () => {
+    const expected = "Basket full, Please choose a bigger basket.";
+    basket.addItem("brownie", 5);
+    let alert = basket.basketCapacity();
+    expect(alert).toEqual(expected);
+  });
 
-  basket.addItem("bagel", 1);
-  basket.addItem("brownie", 3);
-  let result = basket.getBasket();
-  expect(result).toEqual(expected);
-});
+  //Test 4
+  it("Alert when basket is Overflowing", () => {
+    const expected = "Basket full, Please choose a bigger basket.";
+    basket.addItem("bagel", 3);
+    basket.addItem("brownie", 5);
+    let alert = basket.basketCapacity();
+    expect(alert).toEqual(expected);
+  });
 
-//Test 3
-it("Remove bagel from basket", () => {
-  const expected = (this.basket = [
-    { item: "brownie", quantity: 3, price: 3.99 },
-  ]);
+  //Test 5
+  it("Create basket with larger size", () => {
+    const expected = (this.basketSize = largeBasket);
+    new Basket(largeBasket);
+    let checkSize = this.basketSize;
+    expect(checkSize).toEqual(expected);
+  });
 
-  basket.addItem("bagel", 1);
-  basket.addItem("brownie", 3);
-  let removeItem = basket.removeItem("bagel");
-  expect(removeItem).toEqual(expected);
-});
+  //Test 6
+  it("Alert when trying to remove item that doesnt exist inside basket", () => {
+    const expected = "This item is not in the basket.";
+    basket.addItem("bagel", 3);
+    basket.addItem("brownie", 5);
+    let alert = basket.removeItem("Kebab", 10);
+    expect(alert).toEqual(expected);
+  });
 
+  //Test 7
+  it("price checker for items", () => {
+    const expected = 3.99;
+    basket.priceChecker("brownie");
+    let checkPrice = basket.priceChecker("brownie");
+    expect(checkPrice).toEqual(expected);
+  });
 
-it("Alert when basket is full", () => {
-  const expected = "Basket full, Please choose a bigger basket.";
+  //Test 8
+  it("favourite bagel quantity", () => {
+    
+    const favouriteBagel = new Item("chocolateBagel", 1, 4.99);
 
-  basket.addItem("brownie", 5);
-  let alert = basket.basketCapacity();
-  expect(alert).toEqual(expected);
-});
+    const expected = [favouriteBagel, favouriteBagel, favouriteBagel];
 
+    basket.addItem("chocolateBagel", 1);
+    basket.addItem("chocolateBagel", 1);
+    basket.addItem("chocolateBagel", 1);
+    let alert = basket.getBasket();
+    expect(alert).toEqual(expected);
+  });
 
-//Test 4
-it("Alert when basket is Overflowing", () => {
-  const expected = "Basket full, Please choose a bigger basket.";
-
-  basket.addItem("bagel", 3);
-  basket.addItem("brownie", 5);
-  let alert = basket.basketCapacity();
-  expect(alert).toEqual(expected);
-});
-
-//Test 5
-it("Create basket with larger size", () => {
-  const expected = (this.basketSize = largeBasket);
-
-  new Basket(largeBasket);
-  let checkSize = this.basketSize;
-  expect(checkSize).toEqual(expected);
-});
-
-//Test 6
-it("Alert when trying to remove item that doesnt exist inside basket", () => {
-  const expected = "This item is not in the basket.";
-
-  basket.addItem("bagel", 3);
-  basket.addItem("brownie", 5);
-  let alert = basket.removeItem("Kebab", 10);
-  expect(alert).toEqual(expected);
-});
-
-
-//Test 7
-it("price checker for items", () => {
-  const expected = 3.99;
-
-  basket.priceChecker("brownie");
-  let checkPrice = basket.priceChecker("brownie");
-  expect(checkPrice).toEqual(expected);
-});
-
-//Test 8
-it("favourite bagel quantity", () => {
-  const expected = [
-    { item: "chocolateBagel", quantity: 1, price: 4.99 },
-    { item: "chocolateBagel", quantity: 1, price: 4.99 },
-    { item: "chocolateBagel", quantity: 1, price: 4.99 },
-  ];
-
-  basket.addItem("chocolateBagel", 1);
-  basket.addItem("chocolateBagel", 1);
-  basket.addItem("chocolateBagel", 1);
-  let alert = basket.getBasket();
-  expect(alert).toEqual(expected);
-});
-
-//Test 9
-it("basket total", () => {
-  const expected = "£29.93";
-
-  basket.addItem("chocolateBagel", 3);
-  basket.addItem("bagel", 1);
-  basket.addItem("brownie", 3);
-  let total = basket.basketTotal();
-  expect(total).toEqual(expected);
+  //Test 9
+  it("basket total", () => {
+    const expected = "£29.93";
+    basket.addItem("chocolateBagel", 3);
+    basket.addItem("bagel", 1);
+    basket.addItem("brownie", 3);
+    let total = basket.basketTotal();
+    expect(total).toEqual(expected);
+  });
 });
--- src/basket.js
+++ src/basket.js
@@ -6,62 +6,76 @@ const mediumBasket = 10;
 const largeBasket = 15;
 
 class Basket {
-    constructor(capacity = smallBasket) {
-      this.basket = [];
-      this.basketSize = capacity;
-    }
 
-    getBasket() {
-        return this.basket;
-      }
-
-      addItem(itemName, itemQuantity) {
-        let itemFound = false;
-        for (const item in fullMenu) {
-          if (item === itemName) {
-            const newItem = new Item(itemName, itemQuantity, fullMenu[item]);
-            this.basket.push(newItem);
-            itemFound = true;
-            break;
-          }
-        }
-        if (itemFound === false) return "this item does not exist";
-      }
-
-      removeItem(itemName) {
-        for (let i = 0; i < this.basket.length; i++)
-          if (this.basket[i].item === itemName) {
-            this.basket.splice(i, 1);
-            return this.basket;
-          } else if (this.basket[i].item !== itemName)
-            return "This item is not in the basket.";
-      }
-
-      basketCapacity() {
-        const totalCapacity = this.basket.reduce(
-          (total, basketItem) => total + basketItem.quantity,
-          0
-        );
-        if (totalCapacity >= this.basketSize)
-          return "Basket full, Please choose a bigger basket.";
-      }
+  constructor(capacity = smallBasket) {
     
-      priceChecker(itemName) {
-        for (const items in fullMenu)
-          if (itemName === items) return fullMenu[items];
-      }
+    this.basket = [];
+    this.basketSize = capacity;
+  }
 
-      basketTotal() {
-        let eachItem = [];
-        for (let i = 0; i < this.basket.length; i++) {
-          eachItem.push(this.basket[i].quantity * this.basket[i].price);
-        }
-        const totalPrice = eachItem.reduce(
-          (total, quantity) => total + quantity,
-          0
-        );
-        return "£" + totalPrice;
+  getBasket() {
+
+    return this.basket;
+  }
+  
+  addItem(itemName, itemQuantity) {
+
+    let itemFound = false;
+
+    for (const item in fullMenu) {
+
+      if (item === itemName) {
+
+        const newItem = new Item(itemName, itemQuantity, fullMenu[item]);
+        this.basket.push(newItem);
+        itemFound = true;
+        break;
       }
     }
 
-module.exports = Basket
+    if (itemFound === false) return "this item does not exist";
+  }
+
+  removeItem(itemName) {
+
+    const selectedItemIndex = this.basket.findIndex(
+      (bagel) => bagel.name === itemName
+    );
+
+    if (selectedItemIndex === -1) return "This item is not in the basket.";
+    this.basket.splice(selectedItemIndex, 1);
+    return this.basket;
+  }
+
+  basketCapacity() {
+
+    const totalCapacity = this.basket.reduce(
+      (total, basketItem) => total + basketItem.quantity,
+      0
+    );
+    if (totalCapacity >= this.basketSize)
+      return "Basket full, Please choose a bigger basket.";
+  }
+
+  priceChecker(itemName) {
+
+    for (const items in fullMenu)
+      if (itemName === items) return fullMenu[items];
+  }
+
+  basketTotal() {
+
+    let eachItem = [];
+    for (let i = 0; i < this.basket.length; i++) {
+      eachItem.push(this.basket[i].quantity * this.basket[i].price);
+    }
+
+    const totalPrice = eachItem.reduce(
+      (total, quantity) => total + quantity,
+      0
+    );
+    return "£" + totalPrice;
+  }
+}
+
+module.exports = Basket;
