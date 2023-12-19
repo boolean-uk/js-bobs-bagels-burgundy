4682b76c1a597c7ff4b620eff642fcd9c90604b9
Loza_TM <lozatmamo@gmail.com>
2023-12-19 23:08:32 +0100

    menu hs file modified for part three

--- src/menu.js
+++ src/menu.js
@@ -15,13 +15,21 @@ module.exports = Menu */
 const Menu = {
     getMenu: () => {
         return {
-            BGLO: 0.49,
+            /* this code part has been used for part one and twoBGLO: 0.49,
             BGLP: 0.39,
             BGLE: 0.49,
             BGLS: 0.49,
             COF: 0.99,
             BGSE: 2.99,
-            BGSS: 4.99,
+            BGSS: 4.99,*/
+            //for part 3 
+            BGLO: { price: 0.49, name: "Bagel - Onion" },
+            BGLP: { price: 0.39, name: "Bagel - Plain" },
+            BGLE: { price: 0.49, name: "Bagel - Everything" },
+            BGLS: { price: 0.49, name: "Bagel - Sesame" },
+            COF: { price: 0.99, name: "Coffee" },
+            BGSE: { price: 2.99, name: "Bagel Sandwich - Everything" },
+            BGSS: { price: 4.99, name: "Bagel Sandwich - Sesame" },
         };
     },
 };
