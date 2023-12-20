986c1ef9f819524e91f35f88ea1f4bff9f37f263
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2023-12-20 14:52:27 +0100

    comments added  to the basket.js

--- src/basket.js
+++ src/basket.js
@@ -1,18 +1,27 @@
+// Import the MENU module
 const MENU = require('./menu.js')
 
+// Define the Basket class
 class Basket {
+  // Static property defining basket sizes
   static basketSizes = {
     small: 5,
     medium: 10,
     large: 15
   }
+
+  // Constructor method to initialize a new basket with a specified capacity
   constructor(capacity = Basket.basketSizes.small) {
-    this.basket = []
-    this.basketSize = capacity
+    this.basket = [] // Array to store items in the basket
+    this.basketSize = capacity // Current basket size
   }
+
+  // Getter method to retrieve the items in the basket
   getBasket() {
     return this.basket
   }
+
+  // Setter method to set the basket size to a specific value
   setBasketSize(size) {
     if (Object.values(Basket.basketSizes).includes(size)) {
       this.basketSize = size
@@ -20,37 +29,44 @@ class Basket {
       throw new Error('Invalid basket size')
     }
   }
+
+  // Method to add an item to the basket
   addItem(itemName, itemQuantity) {
+    // Check if the basket is full
     if (this.basket.length >= this.basketSize) {
       throw new Error('Basket is full')
     }
 
+    // Retrieve the full menu from the MENU module
     const fullMenu = MENU.GetMenu()
     const selectedMenuItem = fullMenu[itemName]
 
+    // Check if the selected item exists in the menu
     if (selectedMenuItem !== undefined) {
       const insideBasket = {
         item: itemName,
         quantity: itemQuantity,
         price: selectedMenuItem
       }
-      this.basket.push(insideBasket)
+      this.basket.push(insideBasket) // Add the item to the basket
     }
   }
 
+  // Method to remove an item from the basket
   removeItem(itemName) {
     const indexToRemove = this.basket.findIndex(
       (item) => item.item === itemName
     )
 
     if (indexToRemove !== -1) {
-      this.basket.splice(indexToRemove, 1)
+      this.basket.splice(indexToRemove, 1) // Remove the item from the basket
       return this.basket
     } else {
       return 'This item is not in the basket.'
     }
   }
 
+  // Method to check if the basket is over its capacity
   basketCapacity() {
     const totalCapacity = this.basket.reduce(
       (total, item) => total + item.quantity,
@@ -61,11 +77,13 @@ class Basket {
     }
   }
 
+  // Method to check the price of a specific item
   priceChecker(itemName) {
     const fullMenu = MENU.GetMenu()
     return fullMenu[itemName]
   }
 
+  // Method to calculate the total price of all items in the basket
   basketTotal() {
     const totalPrice = this.basket.reduce(
       (total, item) => total + item.quantity * item.price,
@@ -75,4 +93,5 @@ class Basket {
   }
 }
 
+// Export the Basket class for use in other modules
 module.exports = Basket
