0a1649b0947314bb8a94634231b65f068621883d
Tayokanch <tayoquadri11@gmail.com>
2024-01-03 06:56:08 +0000

    grouped the first 3 tests inside a describe

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -9,67 +9,41 @@ describe("Basket", () => {
         basket = new Basket();
     });
 
-    //Test 1
-    it("Get all basket", () => {
-        const expected = []
-        let getBasket = basket.getBasket()
-        expect(getBasket).toEqual(expected)
+    describe('Member of the  public an order a bagel', 'added Item to the basket', 'remove item', ()=>{
+           //Test 1
+        it("Get all basket", () => {
+            const expected = []
+            let getBasket = basket.getBasket()
+            expect(getBasket).toEqual(expected)
+        })
+
+        //Test 2
+        it("Add items to basket", () => {
+            const expected = [
+                { item: "bagel", quantity: 1, price: 2.99 },
+                { item: "brownie", quantity: 3, price: 3.99 }]
+
+            basket.addItem("bagel", 1)
+            basket.addItem("brownie", 3)
+            let bagelInBasket = basket.getBasket()
+            expect(bagelInBasket).toEqual(expected)
+        })
+
+        //Test 3
+        it("Remove bagel from basket", () => {
+            const expected = this.basket = [
+                { item: "brownie", quantity: 3, price: 3.99 }]
+
+            basket.addItem("bagel", 1)
+            basket.addItem("brownie", 3)
+            let removeItem = basket.removeItem("bagel")
+            expect(removeItem).toEqual(expected)
+        })
+
     })
 
-    //Test 2
-    it("Add items to basket", () => {
-        const expected = [
-            { item: "bagel", quantity: 1, price: 2.99 },
-            { item: "brownie", quantity: 3, price: 3.99 }]
 
-        basket.addItem("bagel", 1)
-        basket.addItem("brownie", 3)
-        let bagelInBasket = basket.getBasket()
-        expect(bagelInBasket).toEqual(expected)
-    })
-
-    //Test 3
-    it("Remove bagel from basket", () => {
-        const expected = this.basket = [
-            { item: "brownie", quantity: 3, price: 3.99 }]
-
-        basket.addItem("bagel", 1)
-        basket.addItem("brownie", 3)
-        let removeItem = basket.removeItem("bagel")
-        expect(removeItem).toEqual(expected)
-    })
-
-    //Test 4
-    it("Alert when basket is full", () => {
-        const expected =
-
-            "Basket full, Please choose a bigger basket."
-
-        basket.addItem("bagel", 3)
-        basket.addItem("brownie", 5)
-        let alert = basket.basketCapacity()
-        expect(alert).toEqual(expected)
-    })
-
-    //Test 5
-    it("Create basket with larger size", () => {
-        const expected = this.basketSize = largeBasket
-
-        new Basket(largeBasket)
-        let checkSize = this.basketSize
-        expect(checkSize).toEqual(expected)
-    })
-
-    //Test 6
-    it("Alert when trying to remove item that doesnt exist inside basket", () => {
-        const expected = "This item is not in the basket."
-
-        basket.addItem("bagel", 3)
-        basket.addItem("brownie", 5)
-        let alert = basket.removeItem("Kebab", 10)
-        expect(alert).toEqual(expected)
-    })
-
+   
     //Test 7 
     it("price checker for items", () => {
         const expected = 3.99
